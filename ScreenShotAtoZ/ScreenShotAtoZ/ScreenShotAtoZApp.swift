f4273f235a094f91f75f6576c8917f205b4bc0f1
Raiyan-sharif <raiyan.sharif.1234@gmail.com>
2026-05-04 12:44:29 +0600

    init project

---
+++ ScreenShotAtoZ/ScreenShotAtoZ/ScreenShotAtoZApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenShotAtoZApp.swift
+//  ScreenShotAtoZ
+//
+//  Created by Raiyan Sharif on 4/5/26.
+//
+
+import SwiftUI
+
+@main
+struct ScreenShotAtoZApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
